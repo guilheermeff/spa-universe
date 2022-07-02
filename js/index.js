27457ceedb27472a2ce95bb54eef4424e2823e9c
Guilherme Fernandes <65268723+guilheermeff@users.noreply.github.com>
2022-07-02 19:07:29 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import { Router } from "../js/router.js"
 
 const router = new Router()
 
+router.add("/", "/pages/home.html")
 router.add("/home", "/pages/home.html")
 router.add("/universe", "/pages/universe.html")
 router.add("/exploration", "/pages/exploration.html")
