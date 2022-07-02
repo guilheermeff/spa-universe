8b07bddf1493e5634312478b8aa019029db8d03f
Guilherme Fernandes <65268723+guilheermeff@users.noreply.github.com>
2022-07-02 12:44:53 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { Router } from "../js/router.js"
+
+const router = new Router()
+
+router.add("/home", "/pages/home.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/exploration", "/pages/exploration.html")
+router.add(404, "/pages/404.html")
+
+router.handle()
+
+window.route = () => router.route()
+window.onpopstate = () => router.handle()
+
+console.log(window.location)
